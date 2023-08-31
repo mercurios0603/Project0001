0881b46f60133f4dcdd591943bb91926901a7557
mercurios0603 <mercurios0603@gmail.com>
2023-09-01 08:42:50 +0900

    day2 complete_Triangle Modify

--- day2/Exam16.java
+++ day2/Exam16.java
@@ -2,51 +2,51 @@ package day2;
 
 public class Exam16 {
     public static void main(String[] args) {
-        int y = 3;
+
         // 출력
-        /*
+        /* y = 3;
 
 
-         *
-         ***
+           *
+          ***
          *****
 
 
          */
 
-        y = 5;
+
         // 출력
-        /*
+        /* y = 5;
 
 
-         *
-         ***
-         *****
-         *******
+             *
+            ***
+           *****
+          *******
          *********
 
 
          */
 
-        y = 7;
+
         // 출력
-        /*
+        /* y = 7;
 
 
-         *
-         ***
-         *****
-         *******
-         *********
-         ***********
+               *
+              ***
+             *****
+            *******
+           *********
+          ***********
          *************
 
 
          */
-        int h = 3;
+        int y = 5;
 
-        for(int a = 0; a<h; a++){
-            for(int b = h-1; b > a; b--){
+        for(int a = 0; a<y; a++){
+            for(int b = y-1; b > a; b--){
                 System.out.printf(" ");
             }
             for(int c = 0; c<2*a+1;c++) {
